61e8c7826a31c290c86c689e22b8f41fa506f963
Jacobbidmead <jacob.bidmead.fullstack@outlook.com>
2022-10-05 12:24:43 +0700

    indentation

--- src/App.js
+++ src/App.js
@@ -7,45 +7,41 @@ class App extends React.Component {
 
 
 
-
-render() {
-	return (
-  <div className="background">
-      <nav className="layout">
-        <div>
-          <a href="#" className="button"> <img src="" />Username</a>
-        </div>
-        <div>
-          <div className="title">MagiKards</div>
-        </div>
-        <div>
-          <a href="#" className="button">Quit Game</a>
-        </div>
-      </nav>
-      <div className="layout">
-      <Health />
-
-        <div className="card-table">
-				<Card archetype="ice"/>
-
-
-
-{/*
-	<Card backgroundImage={} title={} description={} />
-// */}
-          <Card archetype="fire"/>
-        </div>
-		
-
-        <Health />
-      </div>
+  	render() {
+			return (
+				<div className="background">
+					<nav className="layout">
+						<div>
+								<a href="#" className="button"> <img src="" />Username</a>
+									</div>
+											<div>
+											<div className="title">MagiKards</div>
+												</div>
+												<div>
+												<a href="#" className="button">Quit Game</a>
+												</div>
+											</nav>
+									<div className="layout">
+								<Health />
+							<div className="card-table">
+						<Card archetype="ice"/>
+					<Card archetype="fire"/>
+				</div>
 
 
-      <Hand />
-    </div>
+				<Health />
+				</div>
 
-	)
- }
+
+				<Hand />
+				</div>
+
+			)
+		}
+
+
+
+
 }
 
 export default App
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -9,25 +9,20 @@ class Card extends React.Component {
 
 	render() {
 		return(
-		
+
 				<div className={`card ${this.props.archetype}`}>
 					<div
 						className="img"
 						style={{backgroundImage: "url('images/iceknight.png')"}}
 					></div>
 					<div className={`card-body ${this.props.archetype}`}>
-						<h5 className="card-title text-center">Frost Warrior</h5>
-						<p className="card-text text-center">
-							<em>
-								A cold wind sweeps over the battle field and damages whoever
-								it touches
-							</em>
-						</p>
-						<p className="card-text text-center atributes">
-							Damage 80<span class="fist-icon"
+						<h3 className="card-title text-center">Frost Warrior</h3>
+
+						<h3 className="card-text text-center atributes">
+							80<span class="fist-icon"
 								><i className={`fa-sharp fa-solid fa-hand-fist ${this.props.archetype}`}></i
 							></span>
-						</p>
+						</h3>
 					</div>
 				</div>
 
