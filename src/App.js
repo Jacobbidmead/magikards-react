2d31bf8392a091bd126333f57b2b6d1d2b450f46
Jacobbidmead <jacob.bidmead.fullstack@outlook.com>
2022-10-05 14:28:22 +0700

    fixed indentation

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import Card from './components/Card/Card.js'
 import HandCard from './components/HandCard/HandCard.js'
 import Health from './components/Health/Health.js'
 
+
+
 class App extends React.Component {
 
 
 
+
+
   	render() {
+
 			return (
 				<div className="background">
 					<nav className="layout">
@@ -31,7 +36,7 @@ class App extends React.Component {
 				<Health />
 				</div>
 
-			
+
 
 				<HandCard />
 				</div>
