301aa79f0622ca501fae0b2b8c5ca6a83c19692c
Jacobbidmead <jacob.bidmead.fullstack@outlook.com>
2022-10-06 13:07:26 +0700

    Added new game component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HandCard from './components/HandCard/HandCard.js'
 import Health from './components/Health/Health.js'
 import {cardsData} from './cardsData'
 import Player from './player'
+import NewGame from './components/NewGame/NewGame.js'
 
 let cards = cardsData
 
@@ -14,7 +15,7 @@ class App extends React.Component {
 		player1: undefined,
 		player2: undefined,
 		status: 'new'
-	
+
 	}
 	initializePlayers = (p1, p2) => {
 		// p1 and p2 above are objects that contain name and player archetype.
@@ -28,15 +29,15 @@ class App extends React.Component {
 		} else if (p2.playerArchetype === 'ice'){
 			p2.health = 700
 		}
-		
+
 		this.setState({
 			player1: new Player(p1),
 			player2: new Player(p2),
 			status: 'battle'
-			
+
 		})
-		
-		
+
+
 	}
 	returnToMenu = () => {
 		this.setState({
@@ -50,7 +51,7 @@ class App extends React.Component {
 			return (
 				<>
 				{/* Players will be populated from the inputs on the 'new game' page */}
-				{this.state.status === 'new' && <div><button onClick={e => this.initializePlayers({name: 'Josh', playerArchetype: 'fire'}, {name: 'Mohammed', playerArchetype: 'ice'})}>click</button>Hello World</div>}
+				{this.state.status === 'new' && <div><NewGame /><button onClick={e => this.initializePlayers({name: 'Josh', playerArchetype: 'fire'}, {name: 'Mohammed', playerArchetype: 'ice'})}>click</button>Hello World</div>}
 				{this.state.status === 'battle' && <div className="background">
 				<nav className="layout">
 					<div>
