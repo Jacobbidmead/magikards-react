ceed795a01b5c38b4ea8b8ab6fe0db9b6740ffe1
Joshua Peoples <46235676+jpcoder12@users.noreply.github.com>
2022-10-07 17:28:27 +0700

    Cards display energy

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -3,7 +3,32 @@ import './Card.css'
 import React from 'react'
 
 class Card extends React.Component {
-
+	swapEnergyVal = (val) => {
+		let icon
+		switch(val){
+			case 0:
+				icon = <i class="fa-solid fa-0"></i>
+				break
+			case 1:
+				icon = <i class="fa-solid fa-1"></i>
+				break
+			case 2:
+				icon = <i class="fa-solid fa-2"></i>
+				break
+			case 3:
+				icon = <i class="fa-solid fa-3"></i>
+				break
+			case 4:
+				icon = <i class="fa-solid fa-4"></i>
+				break
+			case 5:
+				icon = <i class="fa-solid fa-5"></i>
+				break
+			default:
+				return null
+		}
+		return icon
+	}
 
 
 
@@ -16,7 +41,7 @@ class Card extends React.Component {
 						style={{backgroundImage: "url(" +this.props.selectedCard.image+ ")"}}
 					></div>
 					<div className={`card-body ${this.props.selectedCard.archetype}`}>
-						<h3 className="card-title text-center">{this.props.selectedCard.title}</h3>
+						<h3 className="card-title text-center">{this.props.selectedCard.title} {this.swapEnergyVal(this.props.selectedCard.energy)}</h3>
 
 						<h3 className="card-text text-center atributes">
 							{this.props.selectedCard.effects.length == 1 ? <><div>{this.props.selectedCard.effects[0].textValue}<span className="fist-icon"
