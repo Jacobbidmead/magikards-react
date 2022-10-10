a61a05745b872b872d3dd7b097a308f484ad2a79
hamuuri <mohammed_125@outlook.com>
2022-10-10 17:04:26 +0700

    updated the cards

--- src/cardsData.js
+++ src/cardsData.js
@@ -1,391 +1,388 @@
 export const cardsData = [
-    {
-    title: 'Burn',
-    image: '/images/fireball.png',
-    archetype: 'fire',
-    description: 'An intense heat damages',
+  {
+    title: "Burn",
+    image: "/images/fireball.png",
+    archetype: "fire",
+    description: "An intense heat damages",
     energy: 0,
     effects: [
-        {
-            type: 'damage',
-            value: 50,
-            textValue: '50'
-        },
-        {
-          type: 'atkboost',
-          value: 1.35,
-          textValue: '+35%'
-        }
-                ]
-  },
-	{
-    title: 'Basic',
-    image: '/images/sword.png',
-    archetype: 'fire',
-    description: 'An intense heat damages',
-    energy: 0,
-    effects: [
-        {
-            type: 'damage',
-            value: 50,
-            textValue: '50'
-        },
-
-                ]
-  },
-       {
-    title: 'Explosion',
-    image: '/images/sword.png',
-    archetype: 'fire',
-    description: 'An intense heat damages',
-    energy: 2,
-    effects: [
-        {
-            type: 'damage',
-            value: 100,
-            textValue: '100'
-        },
-
-                ]
-  }, {
-    title: 'Fire Dragon',
-    image: '/images/firedragon.png',
-    archetype: 'fire',
-    description: 'Fire Dragon',
-    energy: 3,
-    effects: [
-        {
-            type: 'damage',
-            value: 300,
-            textValue: 300
-        },
-
-                ]
-  }, {
-    title: 'Basic Defend',
-    image: '/images/fireshield.png',
-    archetype: 'fire',
-    description: 'Fire blocked the attack',
-    energy: 0,
-    effects: [
-        {
-            type: 'defense',
-            value: 0.85,
-            textValue: '-15% incoming'
-        },
-
-                ]
-  }, {
-    title: 'Shield',
-    image: '/images/fireshield.png',
-    archetype: 'fire',
-    description: 'Fire blocked with a shield',
-    energy: 1,
-    effects: [
-        {
-            type: 'defense',
-            value: 0.75,
-            textValue: '-25% incoming '
-        },
-
-                ]
-  },{
-    title: 'Evade',
-    image: '/images/fireshield.png',
-    archetype: 'fire',
-    description: 'Fire dodged the attack',
-    energy: 2,
-    effects: [
-        {
-            type: 'defense',
-            value: 0.65,
-            textValue: '-35% incoming'
-        },
-
-                ]
-  },
-    {
-    title: 'Cloak',
-    image: '/images/fireshield.png',
-    archetype: 'fire',
-    description: 'Fire evaded the attack',
-    energy: 3,
-    effects: [
-        {
-            type: 'defense',
-            value: 0.50,
-            textValue: '-50% incoming'
-        },
-
-                ]
-  },{
-    title: 'Basic Boost',
-    image: '/images/firemage.png',
-    archetype: 'fire',
-    description: 'Fire blocked with a shield',
-    energy: 0,
-    effects: [
-        {
-            type: 'damage',
-            value: 50,
-            textValue: '50'
-        },
-        {
-          type: 'atkboost',
-          value: 1.25,
-          textValue: '+25% outgoing'
-        }
-                ]
-  },
-    {
-    title: 'Burn Boost',
-    image: '/images/firemage.png',
-    archetype: 'fire',
-    description: 'Fire gained strength!',
-    energy: 1,
-    effects: [
-        {
-            type: 'damage',
-            value: 50,
-            textValue: '50'
-        },
-        {
-          type: 'atkboost',
-          value: 1.35,
-          textValue: '+35% outgoing'
-        }
-                ]
-  },
-        {
-    title: 'Attack Boost',
-    image: '/images/firemage.png',
-    archetype: 'fire',
-    description: 'Fire gained strength!',
-    energy: 2,
-    effects: [
-        {
-            type: 'damage',
-            value: 200,
-            textValue: '100'
-        },
-        {
-          type: 'atkboost',
-          value: 1.25,
-          textValue: '+25% outgoing'
-        }
-                ]
-  },
-                       {
-    title: 'Fire Mage',
-    image: '/images/firemage.png',
-    archetype: 'fire',
-    description: 'Fire Mage calls fourth his legions',
-    energy: 3,
-    effects: [
-        {
-            type: 'damage',
-            value: 250,
-            textValue: '250'
-        },
-        {
-          type: 'atkboost',
-          value: 1.25,
-          textValue: '+25% outgoing'
-        }
-                ]
+      {
+        type: "damage",
+        value: 50,
+        textValue: "50"
+      },
+      {
+        type: "atkboost",
+        value: 1.35,
+        textValue: "+35%"
+      }
+    ]
   },
   {
-    title: 'Frostbite',
-    image: 'images/sword.png',
-    archetype: 'ice',
-    description: 'An brisk cold damages',
+    title: "Fire Sword",
+    image: "/images/sword.png",
+    archetype: "fire",
+    description: "An intense heat damages",
     energy: 0,
     effects: [
-        {
-            type: 'damage',
-            value: 80,
-            textValue: '80'
-        },
-        {
-          type: 'atkboost',
-          value: 1.35,
-          textValue: '+35% outgoing'
-        }
-                ]
+      {
+        type: "damage",
+        value: 50,
+        textValue: "50"
+      }
+    ]
   },
-	{
-	    title: 'Basic',
-	    image: '/images/sword.png',
-	    archetype: 'ice',
-	    description: '',
-	    energy: 1,
-	    effects: [
-	        {
-	            type: 'damage',
-	            value: 30,
-                textValue: '30'
-	        }
-	                ]
-	  },
-	{
-	    title: 'Cold Burn',
-	    image: '/images/iceknight.png',
-	    archetype: 'ice',
-	    description: '',
-	    energy: 3,
-	    effects: [
-	        {
-	            type: 'damage',
-	            value: 160,
-                textValue: '160'
-	        }
-	                ]
-	  },
-	{
-	    title: 'ICE Dragon',
-	    image: '/images/icedragon.png',
-	    archetype: 'ice',
-	    description: '',
-	    energy: 3,
-	    effects: [
-	        {
-	            type: 'damage',
-	            value: 200,
-                textValue: '200'
-	        }
-	                ]
-	  },
-	{
-	    title: 'Basic Defense',
-	    image: '/images/iceshield.png',
-	    archetype: 'ice',
-	    description: '',
-	    energy: 1,
-	    effects: [
-	        {
-	            type: 'defense',
-	            value: 0.75,
-                textValue: '-25% incoming '
-	        }
-	                ]
-	  },
-	{
-	    title: 'Shield',
-	    image: '/images/iceshield.png',
-	    archetype: 'ice',
-	    description: '',
-	    energy: 2,
-	    effects: [
-	        {
-	            type: 'defense',
-	            value: 0.65,
-                textValue: '-35% incoming '
-	        }
-	                ]
-	  },
-	{
-	    title: 'Evade',
-	    image: '/images/iceshield.png',
-	    archetype: 'ice',
-	    description: '',
-	    energy: 3,
-	    effects: [
-	        {
-	            type: 'defense',
-	            value: 0.5,
-                textValue: '-50% incoming'
-	        }
-	                ]
-	  },
-	{
-	    title: 'Invisibility',
-	    image: '/images/iceshield.png',
-	    archetype: 'ice',
-	    description: '',
-	    energy: 3,
-	    effects: [
-	        {
-	            type: 'defense',
-	            value: 0.3,
-                textValue: '-70% incoming'
-	        }
-	                ]
-	  },
-	{
-	    title: 'Basic Boost',
-	    image: '/images/iceshield.png',
-	    archetype: 'ice',
-	    description: '',
-	    energy: 1,
-	    effects: [
-	        {
-	            type: 'defense',
-	            value: 0.75,
-                textValue: '-25% incoming'
-	        },
-	        {
-	          type: 'atkboost',
-	          value: 1.25,
-              textValue: '+25% outgoing'
-	        }
-	                ]
-	  },
-	{
-	    title: 'Frost Boost',
-	    image: '/images/iceshield.png',
-	    archetype: 'ice',
-	    description: '',
-	    energy: 1,
-	    effects: [
-	        {
-	            type: 'defense',
-	            value: 0.65,
-                textValue: '-35% incoming'
-	        },
-	        {
-	          type: 'atkboost',
-	          value: 1.25,
-              textValue: '+25% outgoing'
-	        }
-	                ]
-	  },
-	                 {
-	    title: 'Attack & defend',
-	    image: '/images/iceknight.png',
-	    archetype: 'ice',
-	    description: '',
-	    energy: 3,
-	    effects: [
-	        {
-	            type: 'damage',
-	            value: 150,
-                textValue: '150'
-	        },
-	        {
-	          type: 'defense',
-	          value: 0.85,
-              textValue: '-15% incoming'
-	        }
-	                ]
-	  },
-	                 {
-	    title: 'Attack & defend',
-	    image: '/images/iceshield.png',
-	    archetype: 'ice',
-	    description: '',
-	    energy: 2,
-	    effects: [
-	        {
-	            type: 'damage',
-	            value: 30,
-                textValue: '30'
-	        },
-	        {
-	          type: 'defense',
-	          value: 0.75,
-              textValue: '-25% incoming'
-	        }
-	                ]
-	  }
-	]
-
+  {
+    title: "Fire Golem",
+    image: "/images/fire_golem.png",
+    archetype: "fire",
+    description: "An intense heat damages",
+    energy: 2,
+    effects: [
+      {
+        type: "damage",
+        value: 100,
+        textValue: "100"
+      }
+    ]
+  },
+  {
+    title: "Fire Dragon",
+    image: "/images/firedragon.png",
+    archetype: "fire",
+    description: "Fire Dragon",
+    energy: 3,
+    effects: [
+      {
+        type: "damage",
+        value: 300,
+        textValue: 300
+      }
+    ]
+  },
+  {
+    title: "Fire Shield",
+    image: "/images/fireshield.png",
+    archetype: "fire",
+    description: "Fire blocked the attack",
+    energy: 0,
+    effects: [
+      {
+        type: "defense",
+        value: 0.85,
+        textValue: "-15% incoming"
+      }
+    ]
+  },
+  {
+    title: "FireBall X Shield",
+    image: "/images/fireballshield.png",
+    archetype: "fire",
+    description: "Fire blocked with a shield",
+    energy: 1,
+    effects: [
+      {
+        type: "defense",
+        value: 0.75,
+        textValue: "-25% incoming "
+      }
+    ]
+  },
+  {
+    title: "Fire Evade",
+    image: "/images/firedodge.png",
+    archetype: "fire",
+    description: "Fire dodged the attack",
+    energy: 2,
+    effects: [
+      {
+        type: "defense",
+        value: 0.65,
+        textValue: "-35% incoming"
+      }
+    ]
+  },
+  {
+    title: "Cloak",
+    image: "/images/firecloak.png",
+    archetype: "fire",
+    description: "Fire evaded the attack",
+    energy: 3,
+    effects: [
+      {
+        type: "defense",
+        value: 0.5,
+        textValue: "-50% incoming"
+      }
+    ]
+  },
+  {
+    title: "Basic Fire Boost",
+    image: "/images/firemage.png",
+    archetype: "fire",
+    description: "Fire blocked with a shield",
+    energy: 0,
+    effects: [
+      {
+        type: "damage",
+        value: 50,
+        textValue: "50"
+      },
+      {
+        type: "atkboost",
+        value: 1.25,
+        textValue: "+25% outgoing"
+      }
+    ]
+  },
+  {
+    title: "Burn Boost",
+    image: "/images/burnboost.png",
+    archetype: "fire",
+    description: "Fire gained strength!",
+    energy: 1,
+    effects: [
+      {
+        type: "damage",
+        value: 50,
+        textValue: "50"
+      },
+      {
+        type: "atkboost",
+        value: 1.35,
+        textValue: "+35% outgoing"
+      }
+    ]
+  },
+  {
+    title: "Attack Fire Boost",
+    image: "/images/fireboost.png",
+    archetype: "fire",
+    description: "Fire gained strength!",
+    energy: 2,
+    effects: [
+      {
+        type: "damage",
+        value: 200,
+        textValue: "100"
+      },
+      {
+        type: "atkboost",
+        value: 1.25,
+        textValue: "+25% outgoing"
+      }
+    ]
+  },
+  {
+    title: "Fire Mage Dragon",
+    image: "/images/firemagedragon.png",
+    archetype: "fire",
+    description: "Fire Mage calls fourth his legions",
+    energy: 3,
+    effects: [
+      {
+        type: "damage",
+        value: 250,
+        textValue: "250"
+      },
+      {
+        type: "atkboost",
+        value: 1.25,
+        textValue: "+25% outgoing"
+      }
+    ]
+  },
+  {
+    title: "Frostbite",
+    image: "images/iceshield.png",
+    archetype: "ice",
+    description: "An brisk cold damages",
+    energy: 0,
+    effects: [
+      {
+        type: "damage",
+        value: 80,
+        textValue: "80"
+      },
+      {
+        type: "atkboost",
+        value: 1.35,
+        textValue: "+35% outgoing"
+      }
+    ]
+  },
+  {
+    title: "Ice Sword",
+    image: "/images/icesword.png",
+    archetype: "ice",
+    description: "",
+    energy: 1,
+    effects: [
+      {
+        type: "damage",
+        value: 30,
+        textValue: "30"
+      }
+    ]
+  },
+  {
+    title: "Cold Burn",
+    image: "/images/iceburn.png",
+    archetype: "ice",
+    description: "",
+    energy: 3,
+    effects: [
+      {
+        type: "damage",
+        value: 160,
+        textValue: "160"
+      }
+    ]
+  },
+  {
+    title: "Ice Dragon",
+    image: "/images/icedragon.png",
+    archetype: "ice",
+    description: "",
+    energy: 3,
+    effects: [
+      {
+        type: "damage",
+        value: 200,
+        textValue: "200"
+      }
+    ]
+  },
+  {
+    title: "Basic Ice Defense",
+    image: "/images/icedefence.png",
+    archetype: "ice",
+    description: "",
+    energy: 1,
+    effects: [
+      {
+        type: "defense",
+        value: 0.75,
+        textValue: "-25% incoming "
+      }
+    ]
+  },
+  {
+    title: "Snow Storm Shield",
+    image: "/images/snowshield.png",
+    archetype: "ice",
+    description: "",
+    energy: 2,
+    effects: [
+      {
+        type: "defense",
+        value: 0.65,
+        textValue: "-35% incoming "
+      }
+    ]
+  },
+  {
+    title: "Icy Evade",
+    image: "/images/iceevade.png",
+    archetype: "ice",
+    description: "",
+    energy: 3,
+    effects: [
+      {
+        type: "defense",
+        value: 0.5,
+        textValue: "-50% incoming"
+      }
+    ]
+  },
+  {
+    title: "Invisibility",
+    image: "/images/iceInvisibility.png",
+    archetype: "ice",
+    description: "",
+    energy: 3,
+    effects: [
+      {
+        type: "defense",
+        value: 0.3,
+        textValue: "-70% incoming"
+      }
+    ]
+  },
+  {
+    title: "Basic Ice Boost",
+    image: "/images/iceboost.png",
+    archetype: "ice",
+    description: "",
+    energy: 1,
+    effects: [
+      {
+        type: "defense",
+        value: 0.75,
+        textValue: "-25% incoming"
+      },
+      {
+        type: "atkboost",
+        value: 1.25,
+        textValue: "+25% outgoing"
+      }
+    ]
+  },
+  {
+    title: "Frost Boost",
+    image: "/images/frostboost.png",
+    archetype: "ice",
+    description: "",
+    energy: 1,
+    effects: [
+      {
+        type: "defense",
+        value: 0.65,
+        textValue: "-35% incoming"
+      },
+      {
+        type: "atkboost",
+        value: 1.25,
+        textValue: "+25% outgoing"
+      }
+    ]
+  },
+  {
+    title: "Mr Freeze",
+    image: "/images/mrfreeze.png",
+    archetype: "ice",
+    description: "",
+    energy: 3,
+    effects: [
+      {
+        type: "damage",
+        value: 150,
+        textValue: "150"
+      },
+      {
+        type: "defense",
+        value: 0.85,
+        textValue: "-15% incoming"
+      }
+    ]
+  },
+  {
+    title: "Winter Soldier",
+    image: "/images/iceattackndefence.png",
+    archetype: "ice",
+    description: "",
+    energy: 2,
+    effects: [
+      {
+        type: "damage",
+        value: 30,
+        textValue: "30"
+      },
+      {
+        type: "defense",
+        value: 0.75,
+        textValue: "-25% incoming"
+      }
+    ]
+  }
+];
